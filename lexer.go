4cb01e7e2f83bba46b54bb9ccdef9bf3b1adc80b
adranwit <adrianwit@gmail.com>
2025-03-06 10:19:03 -0800

    added lexer

---
+++ lexer.go
@@ -0,0 +1,21 @@
+package parsly
+
+type Lexer struct {
+	tokens map[string]*Token
+}
+
+func NewLexer() *Lexer {
+	return &Lexer{tokens: make(map[string]*Token)}
+}
+
+func (l *Lexer) Register(token *Token) {
+	l.tokens[token.Name] = token
+}
+
+func (l *Lexer) Token(name string) *Token {
+	return l.tokens[name]
+}
+
+func (l *Lexer) Tokens() map[string]*Token {
+	return l.tokens
+}
